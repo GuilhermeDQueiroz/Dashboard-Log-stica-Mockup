9daa966adae3bb46a17520f88741830158df129e
Guilherme Diniz Queiroz <gui007k2@gmail.com>
2026-05-12 17:27:35 -0300

    feat

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PainelEstrategico from '../views/PainelEstrategico.vue'
 import PainelOperacional from '../views/PainelOperacional.vue'
 
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
